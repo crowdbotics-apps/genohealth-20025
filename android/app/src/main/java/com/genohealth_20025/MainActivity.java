5245a7588e0edaa91e26778ddb73229f637e01fa
Crowdbotics <team@crowdbotics.com>
2020-09-04 17:07:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/genohealth_20025/MainActivity.java
@@ -0,0 +1,15 @@
+package com.genohealth_20025;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "genohealth_20025";
+    }
+}
